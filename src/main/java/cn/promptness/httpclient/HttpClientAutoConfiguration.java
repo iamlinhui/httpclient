7926e440cd38db56c997c961b286d2ffc3944e5d
lynn <lynn@promptness.cn>
2021-08-08 21:42:50 +0800

    U

--- src/main/java/cn/promptness/httpclient/HttpClientAutoConfiguration.java
+++ src/main/java/cn/promptness/httpclient/HttpClientAutoConfiguration.java
@@ -1,15 +1,14 @@
 package cn.promptness.httpclient;
 
-import java.util.Arrays;
-import java.util.Collections;
-
 import org.apache.http.client.config.AuthSchemes;
-import org.apache.http.client.config.CookieSpecs;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.impl.NoConnectionReuseStrategy;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 
 /**
@@ -17,48 +16,48 @@ import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
  */
 public class HttpClientAutoConfiguration {
 
-	private final HttpClientProperties properties;
+    private final HttpClientProperties properties;
 
-	public HttpClientAutoConfiguration(HttpClientProperties properties) {
-		this.properties = properties;
-	}
+    public HttpClientAutoConfiguration(HttpClientProperties properties) {
+        this.properties = properties;
+    }
 
-	public HttpClientAutoConfiguration() {
-		this.properties = new HttpClientProperties();
-	}
+    public HttpClientAutoConfiguration() {
+        this.properties = new HttpClientProperties();
+    }
 
-	/**
-	 * httpclient bean 的定义
-	 *
-	 * @return CloseableHttpClient
-	 */
-	public CloseableHttpClient httpClient() {
-		return HttpClientBuilder.create().setUserAgent(properties.getAgent())
-				.setConnectionManager(httpClientConnectionManager())
-				.setConnectionReuseStrategy(new NoConnectionReuseStrategy()).build();
-	}
+    /**
+     * httpclient bean 的定义
+     *
+     * @return CloseableHttpClient
+     */
+    public CloseableHttpClient httpClient() {
+        return HttpClientBuilder.create().setUserAgent(properties.getAgent())
+                .setConnectionManager(httpClientConnectionManager())
+                .setConnectionReuseStrategy(new NoConnectionReuseStrategy()).build();
+    }
 
-	public RequestConfig requestConfig() {
-		// 构建requestConfig
-		return RequestConfig.custom().setConnectTimeout(properties.getConnectTimeOut())
-				.setSocketTimeout(properties.getSocketTimeOut())
-				.setConnectionRequestTimeout(properties.getConnectionRequestTimeout())
-				.setCookieSpec(properties.getCookieSpecs()).setExpectContinueEnabled(properties.getExpectContinueEnabled())
-				.setTargetPreferredAuthSchemes(Arrays.asList(AuthSchemes.NTLM, AuthSchemes.DIGEST))
-				.setProxyPreferredAuthSchemes(Collections.singletonList(AuthSchemes.BASIC)).build();
-	}
+    public RequestConfig requestConfig() {
+        // 构建requestConfig
+        return RequestConfig.custom().setConnectTimeout(properties.getConnectTimeOut())
+                .setSocketTimeout(properties.getSocketTimeOut())
+                .setConnectionRequestTimeout(properties.getConnectionRequestTimeout())
+                .setCookieSpec(properties.getCookieSpecs()).setExpectContinueEnabled(properties.getExpectContinueEnabled())
+                .setTargetPreferredAuthSchemes(Arrays.asList(AuthSchemes.NTLM, AuthSchemes.DIGEST))
+                .setProxyPreferredAuthSchemes(Collections.singletonList(AuthSchemes.BASIC)).build();
+    }
 
-	/**
-	 * 定义连接管理器
-	 *
-	 * @return PoolingHttpClientConnectionManager
-	 */
-	private PoolingHttpClientConnectionManager httpClientConnectionManager() {
-		PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
-		connectionManager.setMaxTotal(properties.getMaxConnTotal());
-		connectionManager.setDefaultMaxPerRoute(properties.getMaxConnPerRoute());
-		connectionManager.setValidateAfterInactivity(properties.getValidateAfterInactivity());
-		return connectionManager;
-	}
+    /**
+     * 定义连接管理器
+     *
+     * @return PoolingHttpClientConnectionManager
+     */
+    private PoolingHttpClientConnectionManager httpClientConnectionManager() {
+        PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
+        connectionManager.setMaxTotal(properties.getMaxConnTotal());
+        connectionManager.setDefaultMaxPerRoute(properties.getMaxConnPerRoute());
+        connectionManager.setValidateAfterInactivity(properties.getValidateAfterInactivity());
+        return connectionManager;
+    }
 
 }
