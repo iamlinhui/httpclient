1d923b4a717fbe95ba35d57ee9e6ea8168c07c35
lynn <lynn@holmes.cn>
2026-01-13 19:51:32 +0800

    desc

--- src/main/java/cn/promptness/httpclient/HttpClientProperties.java
+++ src/main/java/cn/promptness/httpclient/HttpClientProperties.java
@@ -3,19 +3,53 @@ package cn.promptness.httpclient;
 import org.apache.http.client.config.CookieSpecs;
 
 public class HttpClientProperties {
+    /**
+     * User-Agent 标识
+     */
     private String agent = "Mozilla/5.0";
+    /**
+     * 连接池最大连接数
+     */
     private Integer maxConnTotal = 200;
+    /**
+     * 每个路由的最大连接数
+     */
     private Integer maxConnPerRoute = 50;
+    /**
+     * 连接超时时间 (毫秒)
+     */
     private Integer connectTimeOut = 10000;
+    /**
+     * 从连接池获取连接的超时时间 (毫秒)
+     */
     private Integer connectionRequestTimeout = 10000;
+    /**
+     * 读取数据超时时间 (毫秒)
+     */
     private Integer socketTimeOut = 50000;
+    /**
+     * 空闲连接校验时间
+     */
     private Integer validateAfterInactivity = 1000;
+    /**
+     * 是否启用 100-continue 机制
+     */
     private Boolean expectContinueEnabled = true;
+    /**
+     * 不自动处理 Set-Cookie 头，也不发送 Cookie
+     */
     private String cookieSpecs = CookieSpecs.IGNORE_COOKIES;
+    /**
+     * 是否启用重定向 (默认禁用重定向)
+     */
     private boolean redirectsEnabled = false;
+    /**
+     * 本地 DNS 映射标签
+     */
     private String ipLabel = "default";
-
-    // 是否忽略 SSL 证书校验 (默认 false，生产环境建议 false，测试环境可设为 true)
+    /**
+     * 是否忽略 SSL 证书校验 (默认 false，生产环境建议 false，测试环境可设为 true)
+     */
     private boolean sslIgnore = false;
 
     public boolean isSslIgnore() {
